b7e9ca3e8d6a9345c98c66615692000d93eab7dd
prima <102790490+primarina314@users.noreply.github.com>
2024-07-21 04:22:04 +0900

    Update and rename Main.cs to main.cs

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using Math;
+
+class Test
+{
+    static void Main(string[] args)
+    {	
+  		for(int i=0;i<10;i++)
+  		{
+  			Functions.CalculatePowerSum(i).show();
+  		}
+    }
+}
